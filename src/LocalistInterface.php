ea631edcbbda5bc77d4f8c0246a196e793cf2514
Alison Jo <2660348+alisonjo315@users.noreply.github.com>
2023-01-26 17:06:21 -0500

    an localist typo; entity entity typo

--- src/LocalistInterface.php
+++ src/LocalistInterface.php
@@ -4,7 +4,7 @@ namespace Drupal\cwd_events_localist_pull;
 use Drupal\Core\Config\Entity\ConfigEntityInterface;
 
 /**
- * Provides an interface defining an localist_pull entity entity.
+ * Provides an interface defining a localist_pull entity.
  */
 interface LocalistInterface extends ConfigEntityInterface {
   // Add get/set methods for your configuration properties here.
